a781f57dbc8751720b463e2a1f8dc28f25b6d028
ATuL <developer.atul96@gmail.com>
2023-05-02 13:38:42 +0530

    1. Remove Storyboard 2. Add Splash Controller 3. Update Git ignore

---
+++ WisdomLeaf/Splash/Controller/SplashVC.swift
@@ -0,0 +1,15 @@
+//
+//  SplashVC.swift
+//  WisdomLeaf
+//
+//  Created by Atul Gupta on 02/05/23.
+//
+
+import UIKit
+
+class SplashVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
